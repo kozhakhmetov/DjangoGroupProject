e4f6760a072caea14050db3fde76e4ed67dbf9c7
Aida Ospanova <ospanovaida08@gmail.com>
2019-12-11 22:23:52 +0600

    Some changes in signals and file_paths

--- project/project/utils/upload.py
+++ project/project/utils/upload.py
@@ -1,18 +1,12 @@
 import os
 import shutil
-from datetime import datetime
 
 
 def document_path(instance, filename):
-    return f'users/{instance.user_id}/{filename}'
-
-
-# def task_delete_path(document):
-#     file_path = document.path
-#     if os.path.isfile(file_path):
-#         os.remove(file_path)
-
+    profile_id = instance.id
+    return f'profile_avatars/{profile_id}/{filename}'
 
 def delete_path(document):
-    datetime_path = os.path.abspath(os.path.join(document.path, '..'))
-    shutil.rmtree(datetime_path)
+    print(document)
+    task_path = os.path.abspath(os.path.join(document.path, '..'))
+    shutil.rmtree(task_path)
