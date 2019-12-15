fafa553130b9f0d60749cc9407871a118e2bbdc9
Aida Ospanova <ospanovaida08@gmail.com>
2019-12-15 21:49:49 +0600

    Add requirements.txt

--- project/project/project/settings.py
+++ project/project/project/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'corsheaders',
+    'django_filters',
     'rest_framework',
     'rest_framework_jwt',
     'users',
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
