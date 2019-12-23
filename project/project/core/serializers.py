80bc88575ce18b236551b27c5cfbfa17a17ba818
Aida Ospanova <ospanovaida08@gmail.com>
2019-12-23 22:03:07 +0600

    Add validators

--- project/project/core/serializers.py
+++ project/project/core/serializers.py
@@ -1,5 +1,6 @@
 from users.serializers import UserSerializer
-from core.models import Post
+from core.models import Post, Comment
+from utils.constants import CATEGORIES
 from rest_framework import serializers
 
 
@@ -17,4 +18,14 @@ class OwnPostSerializer(BasePostSerializer):
         fields = BasePostSerializer.Meta.fields + ('views', 'liked_by')
         read_only_fields = BasePostSerializer.Meta.read_only_fields + ('views', 'liked_by')
 
+    def validate_category(self, category):
+        if category not in CATEGORIES.keys():
+            raise serializers.ValidationError('Category is not correct')
 
+
+class CommentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Comment
+        fields = ('__all__')
+        read_only_fields = ('id', 'created_at', 'created_by')
